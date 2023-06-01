f7996d99cd70f841a3b1b4254425c6159a2e686d
Aftab <uetianaftab@gmail.com>
2023-06-01 11:14:37 +0500

    change url position

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+const Url = "mongodb:aftab:123@ac-lpccfqq-shard-00-00.s863v4d.mongodb.net:27017,ac-lpccfqq-shard-00-01.s863v4d.mongodb.net:27017,ac-lpccfqq-shard-00-02.s863v4d.mongodb.net:27017/FypBackend?replicaSet=atlas-7pi1cf-shard-0&ssl=true&authSource=admin";
 const connectDB = async () => {
-      console.log("mongoose uri", process.env.URL);
+      console.log("mongoose url", Url);
       try {
             const conn = await mongoose.connect(process.env.URL, {
                   useUnifiedTopology: true,
