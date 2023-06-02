bad9694351d774334a3d45ceec464ef49511d446
Aftab <uetianaftab@gmail.com>
2023-06-02 08:21:37 +0500

    configre cors erros

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
-
-
 const express = require('express');
 const path = require('path');
-const cors = require("cors");
-const env = require("dotenv");
-const connectDB = require("./Config/db");
+const cors = require('cors');
+const env = require('dotenv');
+const connectDB = require('./Config/db');
 env.config();
-const crudVoiceRoutes = require("./Routes/crudVoice")
+const crudVoiceRoutes = require('./Routes/crudVoice');
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
+
+// Enable CORS
 app.use(cors({
-      origin: '*',
-      credentials: true,
+  origin: ['https://voice-recording-project-frontend.vercel.app', 'http://localhost:3000'],
+  credentials: true,
 }));
 
 // MongoDb connecting
 connectDB();
 
-
 // Serve uploaded files
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
 // Create a route to handle file uploads
+app.use('/api', crudVoiceRoutes);
 
-app.use("/api", crudVoiceRoutes);
-
-app.get("/", (req, res) => {
-      res.send("Its working perfect")
-})
-
+app.get('/', (req, res) => {
+  res.send('It\'s working perfectly');
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
